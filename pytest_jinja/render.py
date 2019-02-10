61b65bd990a8e853b6ee39200dcc3def675659c0
Karl Hornlund <karlhornlund@gmail.com>
2019-02-10 13:36:17 +1030

    Refactored to use template inheritance. Moved .css and .js to static files

--- pytest_jinja/render.py
+++ pytest_jinja/render.py
@@ -27,33 +27,23 @@ def read_json(json_f):
     sort_by_race = sort_tests(tests, 'random_seed')
     races = [create_race(key, val) for key, val in sort_by_race.items()]
     context['races'] = races
-
     return context
 
+
 def create_race(name, tests):
-    d = {'name': name}
-    d['tests_pass'] = get_passes(tests)
-    d['tests_fail'] = get_fails(tests)
-    d['n_passed'] = len(d['tests_pass'])
-    d['n_failed'] = len(d['tests_fail'])
-    d['n_skipped']: 0  # TODO
-    d['n_errors']: 0  # TODO
-    d['n_xfail']: 0  # TODO
-    d['n_xpass']: 0  # TODO
-    d['n_total'] = len(tests)
+    d = {
+        'name':      name,
+        'tests':     tests,
+        'n_passed':  sum([1 for test in tests if test['result'] == 'passed']),
+        'n_failed':  sum([1 for test in tests if test['result'] == 'failed']),
+        'n_skipped': sum([1 for test in tests if test['result'] == 'skipped']),
+        'n_errors':  sum([1 for test in tests if test['result'] == 'error']),
+        'n_xfail':   sum([1 for test in tests if test['result'] == 'xfail']),
+        'n_xpass':   sum([1 for test in tests if test['result'] == 'xpass']),
+        'n_total':   len(tests)
+    }
     d['all_pass'] = d['n_failed'] == 0
     return d
-
-
-def sort_tests(tests, criteria):
-    groups = {}
-    for test in tests:
-        key = test[criteria]
-        if key in groups.keys():
-            groups[key].append(test)
-        else:
-            groups[key] = [test]
-    return groups
 
 
 def parse_test(test):
@@ -71,6 +61,17 @@ def parse_test(test):
     return d
 
 
+def sort_tests(tests, criteria):
+    groups = {}
+    for test in tests:
+        key = test[criteria]
+        if key in groups.keys():
+            groups[key].append(test)
+        else:
+            groups[key] = [test]
+    return groups
+
+
 def get_tests(data):
     return [parse_test(test) for test in data['tests']]
 
@@ -85,8 +86,8 @@ def get_fails(tests):
 
 if __name__ == '__main__':
     data = read_json('resources/report.json')
-    env = Environment( loader = FileSystemLoader('templates'))
-    template = env.get_template('report_single.html')
+    env = Environment( loader = FileSystemLoader('templates') )
+    template = env.get_template('table-row.html')
 
     out_f = 'rendered.html'
     with open(out_f, 'w') as fh:
---
+++ resources/temp.js
@@ -0,0 +1,25 @@
+<h2>Results</h2>
+    <input 
+        checked="true" 
+        class="filter" 
+        data-prefix-id="_default-" 
+        data-test-result="passed" 
+        hidden="true" 
+        name="filter_checkbox" 
+        onChange="filter_table(this)" 
+        type="checkbox" 
+    /> 
+    <span class="passed">150 passed</span>, 
+    <input 
+        checked="true" 
+        class="filter" 
+        data-prefix-id="_default-" 
+        data-test-result="skipped" 
+        disabled="true" 
+        hidden="true" 
+        name="filter_checkbox" 
+        onChange="filter_table(this)" 
+        type="checkbox" 
+    /> 
+    <span class="skipped">0 skipped</span>, 
+    <input checked="true" class="filter" data-prefix-id="_default-" data-test-result="failed" hidden="true" name="filter_checkbox" onChange="filter_table(this)" type="checkbox" /> <span class="failed">50 failed</span>, <input checked="true" class="filter" data-prefix-id="_default-" data-test-result="error" disabled="true" hidden="true" name="filter_checkbox" onChange="filter_table(this)" type="checkbox" /> <span class="error">0 errors</span>, <input checked="true" class="filter" data-prefix-id="_default-" data-test-result="xfailed" disabled="true" hidden="true" name="filter_checkbox" onChange="filter_table(this)" type="checkbox" /> <span class="xfailed">0 expected failures</span>, <input checked="true" class="filter" data-prefix-id="_default-" data-test-result="xpassed" disabled="true" hidden="true" name="filter_checkbox" onChange="filter_table(this)" type="checkbox" /> <span class="xpassed">0 unexpected passes</span>
